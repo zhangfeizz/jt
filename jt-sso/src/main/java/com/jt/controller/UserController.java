2f56a65ea0f836a7bd3d5c4c743140d9b0f1cefc
userName <zhangfeihpu@163.com>
2019-07-11 11:39:34 +0800

    第二次 注释部分

--- jt-sso/src/main/java/com/jt/controller/UserController.java
+++ jt-sso/src/main/java/com/jt/controller/UserController.java
@@ -20,14 +20,25 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	//前台通过jsonp形式实现跨域请求，后台需要特殊个数封装数据
+	/**
+	  * 前台通过jsonp形式实现跨域请求，后台需要特殊个数封装数据
+	 * @param param
+	 * @param type
+	 * @param callback
+	 * @return
+	 */
 	@RequestMapping("/check/{param}/{type}")
 	public JSONPObject findCheckUser(@PathVariable String param,@PathVariable Integer type, String callback) {
 		Boolean flag = userService.findCheckUser(param,type);
 		return new JSONPObject(callback, SysResult.ok(flag));
 	}
 	
-	//根据token数据获取用户信息   在 index.html 主页面进行回显
+	/**
+	  *  根据token数据获取用户信息   在 index.html 主页面进行回显
+	 * @param token
+	 * @param callback
+	 * @return
+	 */
 	@RequestMapping("/query/{token}")
 	public JSONPObject findUserByToken(@PathVariable String token ,String callback) {
 		String userJson = jedisCluster.get(token);
